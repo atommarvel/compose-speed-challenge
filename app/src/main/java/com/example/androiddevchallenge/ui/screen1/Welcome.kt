1a564facf49dc98f38e6d2cd7a1e49ec154c4347
atommarvel <atom.raiff@gmail.com>
2021-03-13 12:17:44 -0800

    Welcome logo and background

--- app/src/main/java/com/example/androiddevchallenge/ui/screen1/Welcome.kt
+++ app/src/main/java/com/example/androiddevchallenge/ui/screen1/Welcome.kt
@@ -15,7 +15,10 @@
  */
 package com.example.androiddevchallenge.ui.screen1
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
@@ -23,13 +26,18 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.compose.navigate
 import com.example.androiddevchallenge.LocalNavController
 import com.example.androiddevchallenge.Login
+import com.example.androiddevchallenge.R
 import com.example.androiddevchallenge.ui.common.MyButton
 import com.example.androiddevchallenge.ui.common.MyButtonText
 import com.example.androiddevchallenge.ui.theme.MyTheme
@@ -44,26 +52,46 @@ fun WelcomeRoot() {
 @Composable
 fun WelcomeWeTrade(onNavToNextScreen: () -> Unit = {}) {
     MyTheme {
-        Column(
+        Box(
             modifier = Modifier
-                .padding(PaddingValues(16.dp, 32.dp))
-                .navigationBarsPadding()
                 .fillMaxSize(),
-            verticalArrangement = Arrangement.Bottom
+            contentAlignment = Alignment.Center
         ) {
-            Row {
-                MyButton(
-                    onClick = onNavToNextScreen,
-                    modifier = Modifier.weight(1f)
-                ) {
-                    MyButtonText("Get Started")
-                }
-                Spacer(Modifier.width(8.dp))
-                MyButton(
-                    onClick = onNavToNextScreen,
-                    modifier = Modifier.weight(1f)
-                ) {
-                    MyButtonText("Log In")
+            Image(
+                painter = painterResource(R.drawable.ic_welcome_bg),
+                contentDescription = null,
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(MaterialTheme.colors.background),
+                contentScale = ContentScale.FillBounds
+            )
+            Image(
+                painter = painterResource(R.drawable.ic_logo),
+                contentDescription = null,
+                modifier = Modifier
+                    .navigationBarsPadding(),
+            )
+            Column(
+                modifier = Modifier
+                    .padding(PaddingValues(16.dp, 32.dp))
+                    .navigationBarsPadding()
+                    .fillMaxSize(),
+                verticalArrangement = Arrangement.Bottom
+            ) {
+                Row {
+                    MyButton(
+                        onClick = onNavToNextScreen,
+                        modifier = Modifier.weight(1f)
+                    ) {
+                        MyButtonText("Get Started")
+                    }
+                    Spacer(Modifier.width(8.dp))
+                    MyButton(
+                        onClick = onNavToNextScreen,
+                        modifier = Modifier.weight(1f)
+                    ) {
+                        MyButtonText("Log In")
+                    }
                 }
             }
         }
